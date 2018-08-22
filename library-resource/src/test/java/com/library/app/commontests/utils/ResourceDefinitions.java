3dbaaf33d2d8e9b974aa8cdf15805008bb3e457d
Devil <in.krishna@gmail.com>
2018-08-22 18:29:54 +0530

    initial commit

---
+++ library-resource/src/test/java/com/library/app/commontests/utils/ResourceDefinitions.java
@@ -0,0 +1,22 @@
+package com.library.app.commontests.utils;
+
+import org.junit.Ignore;
+
+@Ignore
+public enum ResourceDefinitions {
+	CATEGORY("categories"),
+	AUTHOR("authors"),
+	USER("users"),
+	BOOK("books"),
+	ORDER("orders");
+
+	private String resourceName;
+
+	private ResourceDefinitions(final String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+}
